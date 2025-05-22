aba70748df7d50ba3dabf95f1f17c69e3bc2cbb0
ndikubwimana12 <ndikubwimanaeric2019@gmail.com>
2025-05-22 11:22:51 +0200

    users get

--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,6 @@ router.post('/login', loginUser); // Login
 router.put('/users/:id', updateUser);     // Full update
 router.patch('/users/:id', patchUser);    // Partial update
 router.delete('/users/:id', deleteUser);  // Delete
+router.get('/users', getAllUsers); // Get all users
 
 module.exports = router;
